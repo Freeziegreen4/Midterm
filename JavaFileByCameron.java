86aa822a45a15f007b4d8d27124fe7fdcfd2edb6
= <=>
2025-03-05 12:52:59 -0500

    Initial changes by Cameron

---
+++ JavaFileByCameron.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+
+public class JavaFileByCameron {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\n"
+        + "This is Cameron Wilson's midterm question 2.\n"
+        + "Student Num: 991645161");
+
+        //This is an edit for step 2:
+        System.out.println(LocalDateTime.now());
+    }
+}
